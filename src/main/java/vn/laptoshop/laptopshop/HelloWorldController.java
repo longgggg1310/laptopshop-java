aca208efd0340deb24212f4f9e8d8f830e89377c
Long Vu Dang <74718176+longgggg1310@users.noreply.github.com>
2024-05-13 19:06:14 +0700

    test

---
+++ src/main/java/vn/laptoshop/laptopshop/HelloWorldController.java
@@ -0,0 +1,14 @@
+package vn.laptoshop.laptopshop;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String index() {
+        return "hello";
+    }
+
+}
